afd36c204e478accbdc65203ce366f7a7806f401
Gatorooooo <oscar.lasierra@estudiantat.upc.edu>
2022-01-03 19:45:07 +0100

    first commit

---
+++ lib/app/classifierQuant.dart
@@ -0,0 +1,15 @@
+import 'package:app_prototipo_bioline/app/classifier.dart';
+import 'package:app_prototipo_bioline/common/ops/normalize_op.dart';
+
+class ClassifierQuant extends Classifier {
+  ClassifierQuant({int numThreads: 1}) : super(numThreads: numThreads);
+
+  @override
+  String get modelName => 'models/mobilenet_v1_1.0_224_quant.tflite';
+
+  @override
+  NormalizeOp get preProcessNormalizeOp => NormalizeOp(0, 1);
+
+  @override
+  NormalizeOp get postProcessNormalizeOp => NormalizeOp(0, 255);
+}
